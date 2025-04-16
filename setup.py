d146ad9d191a43e1b2e67539184c174d077aa901
eddiegulay <edgargulay@outlook.com>
2025-04-16 13:19:21 +0200

    fix: Update author email and repository URL in setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="briq",
-    version="0.1.0",
+    version="0.0.7",
     author="Briq Team",
-    author_email="info@briq.tz",
+    author_email="sms@briq.tz",
     description="Python client for the Briq messaging platform API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/briq/briq-python",
+    url="https://github.com/BRIQ-BLOCK/BriqPythonSdk",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
